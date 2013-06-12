ce81da1cac23020f7f4b6d7e6e67d788eba151a7
Geoff Hing <geoffhing@gmail.com>
2013-06-12 12:07:46 -0500

    Clean up post list styles
    Show the full post content instead of the excerpt.
    
    Add some bottom margin to the posts
    
    Put the byline and datestamp on the same line

--- templates/content.php
+++ templates/content.php
@@ -4,7 +4,11 @@
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
-    <?php the_excerpt(); ?>
+		<?php 
+    // Show the full post instead of just the excerpt
+    //the_excerpt(); 
+    the_content();
+    ?>
   </div>
   <footer>
     <?php the_tags('<ul class="entry-tags"><li>','</li><li>','</li></ul>'); ?>
